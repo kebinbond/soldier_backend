9988e02b06d66e11b86d8240573c3720cebd2f97
kevinbond <kevinbond0310@gmail.com>
2023-06-20 22:58:53 +0900

    create appuser model and table

---
+++ app/Models/AppUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppUser extends Model
+{
+    use HasFactory;
+
+    protected $fillabe = [
+        'name',
+        'email',
+        'password',
+        'phone_number'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed'
+    ];
+}
