9a0c51b5a88b9b64e8506b3fdf7caec4c2eb770e
kevinbond <kevinbond0310@gmail.com>
2023-06-23 00:21:14 +0900

    auth page complete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,50 +4,56 @@
     {{-- <div class="col-md-10 text-center ml-auto mr-auto">
         <h3 class="mb-5">Log in to see how you can speed up your web development with out of the box CRUD for #User Management and more.</h3>
     </div> --}}
-    <div class="col-md-7 col-lg-7 ml-auto mr-auto">
-        <form class="form" method="post" action="{{ route('login') }}">
-            @csrf
+    <div class="row">
+        <div class="col-md-7 mr-auto ml-auto">
+            <form class="form mt-5" method="post" action="{{ route('login') }}">
+                @csrf
 
-            <div class="card card-login card-white">
-                <div class="card-header">
-                    <img src="{{ asset('black') }}/img/card-primary.png" alt="">
-                    <h1 class="card-title mt-3 ml-3">{{ __('Log in') }}</h1>
+                <div style="height: 50px; margin-bottom: 50px; justify-content: center; display: flex;">
+                    <img src="{{ asset('black') }}/img/logo.png" alt="" style="height: 50px; width: 100px; margin-right: 20px">
+                    <p style="font-size: 30px; font-weight: bold; letter-spacing: 20px;">VETGUAR<span style="letter-spacing: 0;">D</span></p>
                 </div>
-                <div class="card-body mt-5">
-                    {{-- <p class="text-dark mb-2">Sign in with <strong>admin@black.com</strong> and the password <strong>secret</strong></p> --}}
-                    <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <i class="tim-icons icon-email-85"></i>
-                            </div>
+
+
+
+                <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }} mb-4">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="tim-icons icon-email-85"></i>
                         </div>
-                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}">
-                        @include('alerts.feedback', ['field' => 'email'])
                     </div>
-                    <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">
-                                <i class="tim-icons icon-lock-circle"></i>
-                            </div>
+                    <input type="email" name="email"
+                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                        placeholder="{{ __('Email') }}">
+                    @include('alerts.feedback', ['field' => 'email'])
+                </div>
+                <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }} mb-4">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="tim-icons icon-lock-circle"></i>
                         </div>
-                        <input type="password" placeholder="{{ __('Password') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
-                        @include('alerts.feedback', ['field' => 'password'])
                     </div>
+                    <input type="password" placeholder="{{ __('Password') }}" name="password"
+                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
+                    @include('alerts.feedback', ['field' => 'password'])
                 </div>
-                <div class="card-footer">
-                    <button type="submit" href="" class="btn btn-primary btn-lg btn-block mb-3">{{ __('Get Started') }}</button>
-                    <div class="pull-left">
-                        <h6>
-                            <a href="{{ route('register') }}" class="link footer-link">{{ __('Create Account') }}</a>
-                        </h6>
-                    </div>
-                    <div class="pull-right">
-                        <h6>
-                            <a href="{{ route('password.request') }}" class="link footer-link">{{ __('Forgot password?') }}</a>
-                        </h6>
-                    </div>
+
+                <button type="submit" href=""
+                    class="btn btn-primary btn-lg btn-block mb-3" style="background-image: linear-gradient(to bottom left, #969696, #5b5b5b, #000000);">{{ __('Get Started') }}</button>
+                <div class="pull-left">
+                    <h6>
+                        <a href="{{ route('register') }}" class="link footer-link text-white">{{ __('Create Account') }}</a>
+                    </h6>
                 </div>
-            </div>
-        </form>
+                <div class="pull-right">
+                    <h6>
+                        <a href="{{ route('password.request') }}"
+                            class="link footer-link text-white">{{ __('Forgot password?') }}</a>
+                    </h6>
+                </div>
+
+
+            </form>
+        </div>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,55 +38,65 @@
             </div>
         </div> --}}
         <div class="col-md-7 mr-auto ml-auto">
-            <div class="card card-register card-white">
-                <div class="card-header">
+            {{-- <div class="card card-register card-white"> --}}
+            {{-- <div class="card-header">
                     <img src="{{ asset('black') }}/img/card-primary.png" alt="" style="position: absolute">
                     <h4 class="card-title mt-3 ml-3">{{ __('Register') }}</h1>
-                </div>
-                {{-- <div class="card-header">
+                </div> --}}
+            {{-- <div class="card-header">
                     <img src="{{ asset('black') }}/img/card-primary.png" alt="Card image">
                     <h4 class="card-title">{{ __('Register') }}</h4>
                 </div> --}}
-                <form class="form mt-5" method="post" action="{{ route('register') }}" style="z-index: 1">
-                    @csrf
+            <form class="form mt-5" method="post" action="{{ route('register') }}" style="z-index: 1">
+                @csrf
 
-                    <div class="card-body">
-                        <div class="input-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="tim-icons icon-single-02"></i>
-                                </div>
-                            </div>
-                            <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}">
-                            @include('alerts.feedback', ['field' => 'name'])
+                <div style="height: 50px; margin-bottom: 50px; justify-content: center; display: flex;">
+                    <img src="{{ asset('black') }}/img/logo.png" alt="" style="height: 50px; width: 100px; margin-right: 20px">
+                    <p style="font-size: 30px; font-weight: bold; letter-spacing: 20px;">VETGUAR<span style="letter-spacing: 0;">D</span></p>
+                </div>
+
+                <div class="input-group{{ $errors->has('name') ? ' has-danger' : '' }} mb-4">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="tim-icons icon-single-02"></i>
                         </div>
-                        <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="tim-icons icon-email-85"></i>
-                                </div>
-                            </div>
-                            <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}">
-                            @include('alerts.feedback', ['field' => 'email'])
+                    </div>
+                    <input type="text" name="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                        placeholder="{{ __('Name') }}">
+                    @include('alerts.feedback', ['field' => 'name'])
+                </div>
+                <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }} mb-4">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="tim-icons icon-email-85"></i>
                         </div>
-                        <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="tim-icons icon-lock-circle"></i>
-                                </div>
-                            </div>
-                            <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password') }}">
-                            @include('alerts.feedback', ['field' => 'password'])
+                    </div>
+                    <input type="email" name="email"
+                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                        placeholder="{{ __('Email') }}">
+                    @include('alerts.feedback', ['field' => 'email'])
+                </div>
+                <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }} mb-4">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="tim-icons icon-lock-circle"></i>
                         </div>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <i class="tim-icons icon-lock-circle"></i>
-                                </div>
-                            </div>
-                            <input type="password" name="password_confirmation" class="form-control" placeholder="{{ __('Confirm Password') }}">
+                    </div>
+                    <input type="password" name="password"
+                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                        placeholder="{{ __('Password') }}">
+                    @include('alerts.feedback', ['field' => 'password'])
+                </div>
+                <div class="input-group mb-4">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="tim-icons icon-lock-circle"></i>
                         </div>
-                        {{-- <div class="form-check text-left">
+                    </div>
+                    <input type="password" name="password_confirmation" class="form-control"
+                        placeholder="{{ __('Confirm Password') }}">
+                </div>
+                {{-- <div class="form-check text-left">
                             <label class="form-check-label">
                                 <input class="form-check-input" type="checkbox" value="false">
                                 <span class="form-check-sign"></span>
@@ -94,12 +104,9 @@
                                 <a href="#">{{ __('terms and conditions') }}</a>.
                             </label>
                         </div> --}}
-                    </div>
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary btn-lg btn-block mb-3">{{ __('Get Started') }}</button>
-                    </div>
-                </form>
-            </div>
+                <button type="submit" class="btn btn-primary btn-lg btn-block mb-3" style="background-image: linear-gradient(to bottom left, #969696, #5b5b5b, #000000);">{{ __('Get Started') }}</button>
+            </form>
         </div>
+        {{-- </div> --}}
     </div>
 @endsection
